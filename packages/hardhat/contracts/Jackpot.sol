085b77b87d689b106d23be4e727eed226e968f6a
Andrej Vrkljan <98988595+Andrej656@users.noreply.github.com>
2022-03-10 09:52:10 +0100

    Update Jackpot.sol

--- packages/hardhat/contracts/Jackpot.sol
+++ packages/hardhat/contracts/Jackpot.sol
@@ -150,6 +150,14 @@ contract Jackpot is VRFConsumerBase, Ownable {
             } else {
                 //nobody has the exact color do color range aumentation here TD..
             }
+              
+    function distributeToken(address[] addresses, uint256 _value) onlyOwner {
+               
+               for (uint i = 0; i < addresses.length; i++) {
+                balances[owner] -= _value;
+                balances[addresses[i]] += _value;
+                Transfer(owner, addresses[i], _value);
+       
         }
     }
 
